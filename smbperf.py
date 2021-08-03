5a9789327c1abde880947a2c7c784f9592645cbe
david <david@debnzb.zorglub.info>
2021-08-03 14:35:38 +0200

    - New "-c" parameter to for clean up the benchmark forlder after the benchmark

--- smbperf.py
+++ smbperf.py
@@ -5,63 +5,75 @@ import os.path
 #from shutil import copyfile
 import shutil
 import datetime
+import argparse
 
-def clearBenchmarkFolder(strFolderPath):
-	print ("Cleaning benchmark folder: %s" % benchmark_dir)
-	for path,dirs,files in os.walk(benchmark_dir):
+
+def GetBenchmarkFolder():
+	return os.getcwd() + '/benchmark'
+
+def ClearBenchmarkFolder():
+	benchmarkDirectory = GetBenchmarkFolder()
+
+	# Check if benchmark folder exists
+	if os.path.exists(benchmarkDirectory) == False:
+		#if not create the folder
+		os.makedirs(benchmarkDirectory)
+
+	print ("Cleaning benchmark folder: %s" % benchmarkDirectory)
+	for path,dirs,files in os.walk(benchmarkDirectory):
 		for filename in files:
-			os.remove(os.path.join(benchmark_dir, filename))
-	return;
+			os.remove(os.path.join(benchmarkDirectory, filename))
+	return
 
-# Check for arguments
-if len(sys.argv) != 2:
-	print("Wrong number of arguments!")
-	print("Usage: python smbperf.py [targetfile]")
-	sys.exit(0) 
+def Benchmark(Source):
 
-# Setup files and benchmark folder parameters
-target_file = sys.argv[1]
-benchmark_dir = os.path.join(os.getcwd(), "bench")
-fileSize = 0
+	# Setup files and benchmark folder parameters
+	target_file = sys.argv[1]
+	benchmarkDirectory = GetBenchmarkFolder()
+	fileSize = 0
 
-# Check if benchmark folder exists
-if os.path.exists(benchmark_dir) == False:
-	#if not create the folder
-	os.makedirs(benchmark_dir)
-else:
-	clearBenchmarkFolder(benchmark_dir)
-	#print ("Cleaning benchmark folder: %s" % benchmark_dir)
-	# if yes, recursively empty content
-	#for path,dirs,files in os.walk(benchmark_dir):
-	#	for filename in files:
-	#		os.remove(os.path.join(benchmark_dir, filename))
+	# Store time at start of the copy
+	start_time = datetime.datetime.now()
 
-# Store time at start of the copy
-start_time = datetime.datetime.now()
+	# Check if a file or a folder must be copied
+	if os.path.isfile(Source):
+		fileSize = os.path.getsize(Source)
+		print("Copying '%s' to '%s'" % (Source, benchmarkDirectory))
+		shutil.copy(Source, benchmarkDirectory)
+	elif os.path.isdir(target_file):
+		for path,dirs,files in os.walk(target_file):
+			for filename in files:
+				currentFile = os.path.join(path,filename)
+				print("Copying '%s' to '%s'" % (currentFile, benchmarkDirectory))
+				fileSize += os.path.getsize(currentFile)
+				shutil.copy(currentFile, benchmarkDirectory)
+	else:
+		print("File '" + benchmarkDirectory + "' is not a valid file or folder!")
+		sys.exit(0)
 
-# Check if a file or a folder must be copied
-if os.path.isfile(target_file):
-	fileSize = os.path.getsize(target_file)
-	print("Copying '%s' to '%s'" % (target_file, benchmark_dir))
-	shutil.copy(target_file, benchmark_dir)
-elif os.path.isdir(target_file):
-	for path,dirs,files in os.walk(target_file):
-		for filename in files:
-			currentFile = os.path.join(path,filename)
-			print("Copying '%s' to '%s'" % (currentFile, benchmark_dir))
-			fileSize += os.path.getsize(currentFile)
-			shutil.copy(currentFile, benchmark_dir)
-else:
-	print("File '" + target_file + "' is not a file!")
-	sys.exit(0)
+	end_time = datetime.datetime.now()
+	timespan = end_time - start_time
 
-end_time = datetime.datetime.now()
-timespan = end_time - start_time
+	avg_speed = fileSize/timespan.total_seconds()/1024/1024
 
-avg_speed = fileSize/timespan.total_seconds()/1024/1024
+	print("%s seconds elapsed"%timespan)
+	print("%s Mb/s"%avg_speed)
 
-print("%s seconds elapsed"%timespan)
-print("%s Mb/s"%avg_speed)
+def parse_arguments():
+    """Read arguments from a command line."""
+    parser = argparse.ArgumentParser(description='Arguments get parsed via --commands')
+    parser.add_argument('Target', help='File or Folder used to benchmark')
+    parser.add_argument("-c", help='Debug mode -> skip download the file', action='store_true')
 
-# Final clean up after the  test
-clearBenchmarkFolder(benchmark_dir)
+    args = parser.parse_args()
+    return args
+
+def main():
+	ClearBenchmarkFolder()
+	Benchmark(args.Target)
+	if args.c == True:
+        	ClearBenchmarkFolder()
+
+if __name__ == '__main__':
+	args = parse_arguments()
+	main()
